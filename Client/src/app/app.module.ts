090d394a4a52474ffb0882822b69847e006da8d9
Ramzi Zer <ramzizer@hotmail.com>
2020-12-14 22:32:06 +0100

    nb

---
+++ Client/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';	
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+/* import { BsDropdownModule } from 'ngx-bootstrap/dropdown';*/
+/* import { TooltipModule } from 'ngx-bootstrap/tooltip';*/
+/* import { ModalModule } from 'ngx-bootstrap/modal'; */
+/* import { CollapseModule } from 'ngx-bootstrap/collapse'; */
+
+import { AuthentificationService } from './authentification.service';
+import { ProduitsService } from './produits.service';
+
+import { ConnexionComponent } from './connexion/connexion.component';
+import { ProduitsComponent } from './produits/produits.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { MenuComponent } from './menu/menu.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ConnexionComponent,
+    ProduitsComponent,
+    CategoriesComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [AuthentificationService, ProduitsService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
